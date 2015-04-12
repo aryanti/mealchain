56a2b2bca4e0fba4102519c061ad6c75a5416890
igor27school <igor27school@gmail.com>
2015-04-12 16:43:38 -0700

    Submitting a form
    Adding capability to submit a form with description.

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,17 @@
+import cgi
 from google.appengine.api import users
-
 import webapp2
+
+MAIN_PAGE_HTML = """\
+<html>
+  <body>
+    <form action="/bookdate" method="post">
+      <div><textarea name="content" rows="3" cols="60"></textarea></div>
+      <div><input type="submit" value="Reserve Date"></div>
+    </form>
+  </body>
+</html>
+"""
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
@@ -12,7 +23,14 @@ class MainPage(webapp2.RequestHandler):
         self.response.write('Hello, ' + user.nickname())
     else:
         self.redirect(users.create_login_url(self.request.uri))
+        
+class BookDate(webapp2.RequestHandler):
+    def post(self):
+        self.response.write('<html><body>Your comment:<pre>')
+        self.response.write(cgi.escape(self.request.get('content')))
+        self.response.write('</pre></body></html>')
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/bookdate', BookDate),
 ], debug=True)
